5460309d51c02bda17191bd535a85e6e26621451
uptonking <jinyaoo86@gmail.com>
2020-08-22 17:16:12 +0800

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# react-quickstart-typescript
+
+> quickstart boilerplate for react using typescript
+
+## features
+
+- almost minimal
+- built with react + typescript + babel + webpack from scratch
+- support react fast refresh(hot reloading)
+
+## quickstart
+
+``` shell
+yarn 
+yarn run demo
+```
+- open http://localhost:8999/
---
+++ src/render.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+
+const render = Component => {
+  ReactDOM.render(<Component />, document.getElementById('app'));
+};
+render(App);
+
+if ((module as any).hot) {
+  (module as any).hot.accept('./App.tsx', () => {
+    render(App);
+  });
+}
